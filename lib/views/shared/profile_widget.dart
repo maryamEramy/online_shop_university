c95551e6daed337311842704d59c6800eb598659
maryam <eramy.mary@gmail.com>
2025-09-02 01:40:10 +0330

    fix profile page ui

--- lib/views/shared/profile_widget.dart
+++ lib/views/shared/profile_widget.dart
@@ -1,15 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uni_online_shop/controllers/constant.dart';
 
 import '../../controllers/image_path.dart';
 import '../ui/profile_page.dart';
 import '../ui/registration_page.dart';
 
 class ProfileWidget extends StatelessWidget {
-  const ProfileWidget({
-    super.key,
-    required this.user,
-  });
+  const ProfileWidget({super.key, required this.user});
 
   final User? user;
 
@@ -18,79 +16,56 @@ class ProfileWidget extends StatelessWidget {
     final User? user = FirebaseAuth.instance.currentUser;
 
     return SizedBox(
-      child: user != null ?
-      Column(
-        children: [
-          ListTile(
-            leading: Container(
-              width: 60,
-              height: 60,
-              // padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(2000),
-                ),
-              ),
-              child: Center(
-                child: Image(
-                  image: AssetImage(ImagePath.womanProfile),
-                  // fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            title: Text(user.displayName ?? "no name",),
-            subtitle: Text(
-              "${user.email}",),
-            trailing: IconButton( icon: Icon(Icons.logout),
-              onPressed: ()async{
-                await FirebaseAuth.instance.signOut();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProfilePage(),
+      child:
+          user != null
+              ? ListTile(
+                leading: Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(2000)),
                   ),
-                );
-              },
-              color: Colors.red, ),
-          ),
-
-        ],
-      )
-          : Column(
-        children: [
-          ListTile(
-            leading: Container(
-              width: 60,
-              height: 60,
-              // padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(2000),
-                ),
-              ),
-              child: Center(
-                child: Image(color: Colors.amber,
-                  image: AssetImage(ImagePath.womanProfile,),
-                  // fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            title: Text("you have no account",),
-            trailing: IconButton( icon: Icon(Icons.login),
-              onPressed: ()async{
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Registration_page(),
+                  child: Center(
+                    child: Image(
+                      image: AssetImage(ImagePath.womanProfile),
+                      // fit: BoxFit.cover,
+                    ),
                   ),
-                );
-              },
-              color: Colors.red, ),
-          ),
-        ],
-      ),
+                ),
+                title: Text(user.displayName ?? "no name" , style: kMainTextStyle,),
+                subtitle: Text("${user.email}" , style: kSecondTextStyle,),
+              )
+              : ListTile(
+                leading: Container(
+                  width: 90,
+                  height: 90,
+                  // padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(2000)),
+                  ),
+                  child: Center(
+                    child: Image(
+                      color: kSecondaryColor,
+                      image: AssetImage(ImagePath.womanProfile),
+                      // fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                title: Text("you have no account" , style: kMainTextStyle,),
+                trailing: IconButton(
+                  icon: Icon(Icons.login),
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Registration_page(),
+                      ),
+                    );
+                  },
+                ),
+              ),
     );
   }
 }
--- lib/views/ui/profile_page.dart
+++ lib/views/ui/profile_page.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:uni_online_shop/controllers/image_path.dart';
+import 'package:uni_online_shop/controllers/constant.dart';
+import 'package:uni_online_shop/views/shared/body_ui.dart';
 import 'package:uni_online_shop/views/shared/roundedButton.dart';
 import 'package:uni_online_shop/views/ui/cart_page.dart';
 import 'package:uni_online_shop/views/ui/favorites_page.dart';
-import 'package:uni_online_shop/views/ui/registration_page.dart';
-import 'package:uni_online_shop/views/ui/signup_page.dart';
 import '../shared/appstyle.dart';
 import '../shared/profile_widget.dart';
 
@@ -20,55 +19,72 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     final User? user = FirebaseAuth.instance.currentUser;
-    return Scaffold(
-      backgroundColor: const Color(0xFFE2E2E2),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ClipPath(
-              child: SizedBox(
-                height: 600,
-                child: Stack(
+    return BodyUi(
+      headerTitle: 'My Profile',
+      children: [
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              ProfileWidget(user: user),
+              SizedBox(height: 90),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: kLightPrimaryColor,
+                ),
+                child: Column(
                   children: [
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(16, 20, 0, 0),
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/logo/top_of_screen.png"),
-                          alignment: Alignment.topCenter,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Text(
-                          'My Profile',
-                          style: appstyle(36, Colors.black, FontWeight.bold),
-                        ),
-                      ),
+                    RoundedButton(
+                      title: "Favorites",
+                      color: Colors.transparent,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FavoritesPage(),
+                          ),
+                        );
+                      },
+                      textColor: kWhiteColor,
                     ),
-                    Column(
-                      children: [
-                        SizedBox(height: 90),
-                        ProfileWidget(user: user),
-                        SizedBox(height: 90),
-                        RoundedButton(title: "Favorites", color: Colors.white70, onPressed: (){
-                          Navigator.push(context,  MaterialPageRoute(builder: (context) => FavoritesPage()));
-                        }, textColor: Colors.black,),
-                        RoundedButton(title: "Your Basket", color: Colors.white70, onPressed: (){
-                          Navigator.push(context,  MaterialPageRoute(builder: (context) => CartPage()));
-                        }, textColor: Colors.black,)
-                      ],
+                    RoundedButton(
+                      title: "Your Basket",
+                      color: Colors.transparent,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CartPage()),
+                        );
+                      },
+                      textColor: kWhiteColor,
+                    ),
+                    RoundedButton(
+                      title: "Questions",
+                      color: Colors.transparent,
+                      onPressed: () {},
+                      textColor: kWhiteColor,
+                    ),
+                    RoundedButton(
+                      title: "LogOUT",
+                      color: Colors.transparent,
+                      onPressed: () {
+                        FirebaseAuth.instance.signOut();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProfilePage(),
+                          ),
+                        );
+                      },
+                      textColor: kWhiteColor,
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
-
