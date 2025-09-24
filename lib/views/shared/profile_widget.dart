ad1b996b4bc258f89ae7a2569557e19f9bc60ff6
maryam <eramy.mary@gmail.com>
2025-09-24 16:56:06 +0330

    set profile photo => select photo from user's gallery

--- lib/views/shared/profile_widget.dart
+++ lib/views/shared/profile_widget.dart
@@ -1,127 +1,107 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:uni_online_shop/controllers/constant.dart';
 import '../../controllers/image_path.dart';
-class ProfileWidget extends StatelessWidget {
+
+class ProfileWidget extends StatefulWidget {
   const ProfileWidget({super.key, required this.user});
 
   final User? user;
 
   @override
+  State<ProfileWidget> createState() => _ProfileWidgetState();
+}
+
+class _ProfileWidgetState extends State<ProfileWidget> {
+  File? _imageFile;
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+
+      //upload image to Firebase Storage
+      //then
+      //save the URL in user's profile
+      // uploadToFirebase(_imageFile);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // از user پاس داده شده استفاده کنید
-    final currentUser = user;
+    final currentUser = widget.user;
 
     return Row(
       children: [
         currentUser != null
             ? Row(
-          children: [
-            Container(
-              width: 90,
-              height: 90,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(1000),
-              ),
-              child: Center(
-                child: Image(image: AssetImage(ImagePath.womanProfile)),
-              ),
-            ),
-            SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(currentUser.displayName ?? "no name", style: kMainTextStyle),
-                Text("${currentUser.email}", style: kSecondTextStyle),
+                GestureDetector(
+                  onTap: _pickImage,
+                  child: Container(
+                    width: 90,
+                    height: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(1000),
+                    ),
+                    child: Center(
+                      child:
+                          _imageFile != null
+                              ? ClipRRect(
+                                borderRadius: BorderRadius.circular(1000),
+                                child: Image.file(
+                                  _imageFile!,
+                                  width: 90,
+                                  height: 90,
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                              : Image(
+                                image: AssetImage(ImagePath.womanProfile),
+                              ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      currentUser.displayName ?? "no name",
+                      style: kMainTextStyle,
+                    ),
+                    Text("${currentUser.email}", style: kSecondTextStyle),
+                  ],
+                ),
+              ],
+            )
+            : Row(
+              children: [
+                Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(1000),
+                  ),
+                  child: Center(
+                    child: Image(
+                      color: kSecondaryColor,
+                      image: AssetImage(ImagePath.womanProfile),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text("you have no account", style: kMainTextStyle),
               ],
             ),
-          ],
-        )
-            : Row(
-          children: [
-            Container(
-              width: 90,
-              height: 90,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(1000),
-              ),
-              child: Center(
-                child: Image(
-                  color: kSecondaryColor,
-                  image: AssetImage(ImagePath.womanProfile),
-                ),
-              ),
-            ),
-            SizedBox(width: 10),
-            Text("you have no account", style: kMainTextStyle),
-          ],
-        ),
       ],
     );
   }
 }
-
-// class ProfileWidget extends StatelessWidget {
-//   const ProfileWidget({super.key, required this.user});
-//
-//   final User? user;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//
-//     final User? user = this.user; // بجای FirebaseAuth.instance.currentUser
-//
-//     // final User? user = FirebaseAuth.instance.currentUser;
-//
-//     return Row(
-//       children: [
-//         user != null
-//             ? Row(
-//               children: [
-//                 Container(
-//                   width: 90,
-//                   height: 90,
-//                   decoration: BoxDecoration(
-//                     color: Colors.white,
-//                     borderRadius: BorderRadius.circular(1000),
-//                   ),
-//                   child: Center(
-//                     child: Image(image: AssetImage(ImagePath.womanProfile)),
-//                   ),
-//                 ),
-//                 SizedBox(width: 10,),
-//                 Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     Text(user.displayName ?? "no name", style: kMainTextStyle),
-//                     Text("${user.email}", style: kSecondTextStyle),
-//                   ],
-//                 ),
-//               ],
-//             )
-//             : Row(
-//               children: [
-//                 Container(
-//                   width: 90,
-//                   height: 90,
-//                   decoration: BoxDecoration(
-//                     color: Colors.white,
-//                     borderRadius: BorderRadius.circular(1000),
-//                   ),
-//                   child: Center(
-//                     child: Image(
-//                       color: kSecondaryColor,
-//                       image: AssetImage(ImagePath.womanProfile),
-//                     ),
-//                   ),
-//                 ),
-//                 SizedBox(width: 10,),
-//                 Text("you have no account", style: kMainTextStyle),
-//               ],
-//             ),
-//       ],
-//     );
-//   }
-// }
